6ae12c43ff06c99bcd1e5236ebeb1569dceca500
Aditi <aditibupadhyay@gmail.com>
2026-08-10 23:47:18 +0530

    Enhance rotateArray90degree function to handle empty matrices and refactor result array initialization

--- array/rotateArray90degree.js
+++ array/rotateArray90degree.js
@@ -1,7 +1,9 @@
 function rotateArray90degree(matrix) {
+    if (!matrix.length || !matrix[0].length) return [];
+
     let rows = matrix.length;
     let columns = matrix[0].length;
-    let result = new Array(columns).fill(0).map(() => new Array(rows).fill(0));
+    let result = Array.from({ length: columns }, () => Array(rows));
     for(let i = 0; i<matrix.length; i++) {
         for(let j = 0; j<matrix[i].length; j++) {
             result[j][rows - i - 1] = matrix[i][j];
