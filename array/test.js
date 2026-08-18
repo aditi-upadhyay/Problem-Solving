a9b677ba5c64ecce31055f23f3b34636769672c6
Aditi <aditibupadhyay@gmail.com>
2026-08-18 23:24:59 +0530

    Refactor bubble sort function for improved readability and consistency in formatting

--- array/test.js
+++ array/test.js
@@ -1,14 +1,13 @@
-function test(input){
-        for(let i = 0;i<input.length;i++){
-            for(let j = i+1;j<input.length;j++){
-                if(input[i] > input[j]){
-                    let temp = input[i]
-                    input[i] = input[j]
-                    input[j] = temp
-                }
-            }
-        }
-        return input
+function test(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] > input[j]) {
+        let temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+      }
     }
-     
-    console.log(test([1,0,2,1,0]))
+  }
+  return input;
+}
+console.log(test([1, 0, 2, 1, 0]));
