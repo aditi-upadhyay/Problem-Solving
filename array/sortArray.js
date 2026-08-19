3b4572f7139c6c7daf1bc72f602d2d1311d23622
Aditi <aditibupadhyay@gmail.com>
2026-08-19 23:06:06 +0530

    Add function to sort an array of 0s, 1s, and 2s in non-decreasing order

--- array/sortArray.js
+++ array/sortArray.js
@@ -1,3 +1,9 @@
+/** Sort an array of 0s, 1s and 2s
+Problem Statement: Given an array nums consisting of only 0, 1, or 2. 
+Sort the array in non-decreasing order. The sorting must be done in-place, 
+without making a copy of the original array. **/
+
+
 function test(input) {
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; j < input.length; j++) {
