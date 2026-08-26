3d6310a80ab211a79c602ea0f754a7155dd6456f
Aditi <aditibupadhyay@gmail.com>
2026-08-26 22:52:28 +0530

    program to finad maximum repeated element from array

---
+++ array/majorityElement.js
@@ -0,0 +1,15 @@
+function majorityElement(input){
+    let hashMap = {}
+    for(let i=0;i<input.length;i++){
+        if(hashMap.hasOwnProperty(input[i])){
+            hashMap[input[i]] += 1
+            if(hashMap[input[i]] === Math.round(input.length/2)) {
+                return input[i]
+            }
+        }else{
+            hashMap[input[i]] = 1
+        }
+    }
+}
+
+console.log(majorityElement([7, 0, 0, 1, 7, 7, 2, 7, 7]))
